9ca1987b4f49aab3df14a2318557747b5cc1b64c
Negar <negar.nbj96@yahoo.com>
2019-08-06 22:43:53 +0430

    add bar plot visualization

--- goodreads/calcAverageRating.py
+++ goodreads/calcAverageRating.py
@@ -1,5 +1,7 @@
 from __future__ import division
 import json
+import matplotlib.pyplot as plt
+import numpy as np
 
 class AverageCalculator(object):
     def __init__(self, address):
@@ -7,6 +9,7 @@ class AverageCalculator(object):
             data = myFile.read()
             self.editionsData = json.loads(data)
             self.calculatedAvgs = {}
+            self.finalAvgs = {}
 
     def calcAverage(self):
         for edition in self.editionsData:
@@ -28,13 +31,27 @@ class AverageCalculator(object):
         for language in self.calculatedAvgs:
             if(self.calculatedAvgs[language]['numOfRaters'] > 100):
                 average = self.calculatedAvgs[language]['weightedSum'] / self.calculatedAvgs[language]['numOfRaters']
+                self.finalAvgs[language] = average
                 print(language + ' : ' + str(average)) 
+    
+    def plot_bar_x(self, bookName):
+        # this is for plotting purpose
+        plt.xlabel('Edition Language', fontsize=10)
+        plt.ylabel('Average Rating', fontsize=10)
+        plt.title('Average rating vs Language for ' + bookName)
+        plt.bar(range(len(self.finalAvgs)), self.finalAvgs.values(), align='center', width=0.5)  # python 2.x
+        plt.xticks(range(len(self.finalAvgs)), self.finalAvgs.keys(), fontsize = 4)  # in python 2.x
+        plt.show()
+
+plt.show()
+        
         
 
 
 def main():
     ac = AverageCalculator('editionsData.json')
     ac.calcAverage()
+    ac.plot_bar_x('One Hundred years of solitude')
 
 
 if __name__ == "__main__":
