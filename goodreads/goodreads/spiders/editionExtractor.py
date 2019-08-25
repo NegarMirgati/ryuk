bf8f8861a64cc31682d830b4d3ba7f58d700fa8f
Negar <negar.nbj96@yahoo.com>
2019-08-26 01:54:08 +0430

    implement extracting each book's edition links from a listopia

--- goodreads/goodreads/spiders/editionExtractor.py
+++ goodreads/goodreads/spiders/editionExtractor.py
@@ -1,26 +1,39 @@
 import scrapy
 import urlparse
-from goodreads.items import BookEditionsItem
+import json
+from goodreads.items import BookListEditions
+from scrapy.loader import ItemLoader
 
-class ReviewsSpider(scrapy.Spider):
+class EditionSpider(scrapy.Spider):
     name = "edition_extractor"
-    editions = BookEditionsItem()
-    editions['urls'] = []
-    start_urls = [
-        'https://www.goodreads.com/work/editions/3295655-cien-a-os-de-soledad'
-    ]
+    list_editions = BookListEditions()
+    list_editions['dic'] = {}
+
+    with open('links.json') as json_file:
+        data = json.load(json_file)
+        start_urls = data[0]['bookUrls']
 
     def parse(self, response):
-        self.editions['name'] = response.css('div.mainContentFloat h1 a::text').extract_first().strip()
-        # self.editions['urls'] = []
-            
-        # editions['urls'].append(self.extract_urls(response))
-        
+        editions = {'name' : '' }
+        editons_page = response.xpath("//div[@class='otherEditionsActions']/a[@class='actionLinkLite'][1]/@href").extract()
+        if(len(editons_page) > 0):
+            url = urlparse.urljoin(response.url, editons_page[0])
+            request = scrapy.Request(url, callback = self.parse_editions_url)
+            request.meta['editions'] = editions
+            yield request
+
+    def parse_editions_url(self, response):
+        name = response.css('div.mainContentFloat h1 a::text').extract_first().strip()
+        editions = response.meta['editions']
+        if(name not in editions['name']):
+            editions['name'] = name
+            editions['urls'] = []
+
         for book_page in response.css('a.bookTitle').xpath('@href'):
-            self.editions['urls'].append(urlparse.urljoin(response.url, book_page.extract()))
-        
+            editions['urls'].append((urlparse.urljoin(response.url, book_page.extract())))
+                
         next_page = response.xpath("//*[@rel='next']/@href").extract_first()
         if next_page is not None:
-            yield response.follow(next_page, callback=self.parse)
+            yield response.follow(next_page, callback = self.parse_editions_url, meta = {'editions' : editions})
         else:
-            yield self.editions
+            yield editions
